68270942c23ee36164e28fe3481e0fe259a669df
Tapu45 <rameswarpanda891@gmail.com>
2025-08-14 00:40:52 +0530

    bakchodigyguyfhfh

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,9 +16,11 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-unused-expressions": "off", // <-- Add this line
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-this-alias": "off", // <-- Add this line
     },
   },
 ];
 
 export default eslintConfig;
+
