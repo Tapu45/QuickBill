fb372974b95eeb1bdca94ef773db86636357236f
Tapu45 <rameswarpanda891@gmail.com>
2025-08-14 00:48:33 +0530

    hhu

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  eslint: {
+    ignoreDuringBuilds: true, // <-- disables ESLint during build
+  },
   typescript: {
     ignoreBuildErrors: true, // <-- Add this line
   },
